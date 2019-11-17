da80594588585b93ded0cf10671558bebb7b1e2f
Abner Nascimento <abnersousanascimento@gmail.com>
2019-11-16 22:46:58 -0300

    Add implementation to compute most frequent features, after clusterizing them with DBSCAN.

--- orb.py
+++ orb.py
@@ -3,38 +3,51 @@ import cv2 as cv
 from PIL import Image
 from cv2.cv2 import DescriptorMatcher
 from scipy.spatial.distance import hamming
+from scipy.stats import mode
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import pairwise_fast, pairwise_distances
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
+
+def hamming_pairwise_distances(X):
+    """
+    Computes hamming distances between each sample of X
+    :param X: List of orb descriptors in decimal form.
+    :return: Hamming distance between each descriptor.
+    """
+    # Unpacks bits for matrix a, forming a MxN matrix
+    X_bin = np.unpackbits(X, axis=1)
+
+    # Broadcasts the matrix to MxNx1 and 1xNxM so that each lines of the
+    # first matrix can be XORd to each line of the second along the 3rd dimension.
+    X_bin = X_bin[:, :, None] ^ X_bin.T[None, :, :]
+    return X_bin.sum(axis=1)
+
 # Loads image
-image = np.array(Image.open('samples/img1.jpg'))
+image = np.array(Image.open('samples/portinari.jpg'))
 
 # Computes features
-extractor = cv.ORB_create(nfeatures=2000)
+extractor = cv.ORB_create(nfeatures=500)
 kps = extractor.detect(image)
+_, dsc = extractor.compute(image, kps)
+
 kps = np.array([k.pt for k in kps])
 
-# Compute distances
-distances = pairwise_distances(kps, kps)
+# Computes distances between each descriptor
+distances = hamming_pairwise_distances(dsc)
 
-# Clusterizes
-margin = np.quantile(distances, 0.005)
+# Clusterize by DBSCAN
+margin = np.quantile(distances, 0.06)
 clusterizer = DBSCAN(eps=margin, metric='precomputed')
 labels = clusterizer.fit_predict(distances)
 
-# Compute cluster centroids
-centroids = []
-for label in np.unique(labels):
-    if label == -1:
-        continue
-    cluster = kps[labels == label]
-    centroids.append(cluster.mean(axis=0))
-centroids = np.array(centroids)
+# Gets features from largest cluster and ignore the rest
+kps = kps[labels == mode(labels)[0]]
+labels = labels[labels == mode(labels)[0]]
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.imshow(image)
-ax.scatter(centroids[:, 0], centroids[:, 1])
+ax.scatter(kps[:, 0], kps[:, 1], c=labels)
 plt.show()
